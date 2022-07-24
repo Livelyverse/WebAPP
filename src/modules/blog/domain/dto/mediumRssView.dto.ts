20cdd3a8c27a2e1e61cf88ddfc7679e25cb38afd
Sina Tadayon <sina.tadayyon@gmail.com>
2022-07-24 14:14:12 +0430

    fixed thumbnail of api issue

--- src/modules/blog/domain/dto/mediumRssView.dto.ts
+++ src/modules/blog/domain/dto/mediumRssView.dto.ts
@@ -21,7 +21,8 @@ export class MediumRssViewDto {
       rssViewDto.description= blog.description;
       rssViewDto.image= blog.image;
       rssViewDto.item= blog.feed;
-      rssViewDto.protocol= blog.protocol === ProtocolType.RSS ? "RSS" : "ATOM";
+      rssViewDto.thumbnail = blog.thumbnail;
+      rssViewDto.protocol= blog.protocol;
       rssViewDto.version= blog.protocolVersion;
       return rssViewDto;
     }
@@ -42,6 +43,9 @@ export class MediumRssViewDto {
 
   @ApiProperty()
   public title: string;
+
+  @ApiProperty()
+  public thumbnail: string;
 
   @ApiProperty()
   public feedUrl: string;
