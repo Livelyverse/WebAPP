195cbdd7e22baa305f0ffa99cc0c507c91f5dc28
Sina Tadayon <sina.tadayyon@gmail.com>
2023-03-26 05:37:33 +0330

    activatd all airdrop jobs

--- src/modules/airdrop/airdrop.module.ts
+++ src/modules/airdrop/airdrop.module.ts
@@ -59,13 +59,13 @@ import { DiscordMemberJob } from "./domain/jobs/discord/discordMembers.job";
     AirdropRuleService,
     AirdropService,
     AirdropScheduleService,
-    // TwitterFollowerJob,
-    // TweetTrackerJob,
+    TwitterFollowerJob,
+    TweetTrackerJob,
     SocialAirdropJob,
-    // InstagramPostTrackerJob,
-    // InstagramFollowerJob,
-    // TelegramSubscriberJob,
-    // DiscordMemberJob
+    InstagramPostTrackerJob,
+    InstagramFollowerJob,
+    TelegramSubscriberJob,
+    DiscordMemberJob
   ],
 })
 export class AirdropModule {}
